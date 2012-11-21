95781e1cdb687c63b8add4d565d1f828b6418e67
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-11-21 21:26:26 +0200

    Adding some logging.

--- lib/event-store/hooks/deferred-dispatcher-hook.rb
+++ lib/event-store/hooks/deferred-dispatcher-hook.rb
@@ -1,5 +1,7 @@
 module EventStore::Hooks
   class DeferredDispatcherHook < PipelineHook
+    Log = EventStore::Logging::Logger.get 'event-store::deferred-dispatcher-hook'
+    
     def initialize(dispatcher_hook)
       @dispatcher_hook = dispatcher_hook
       @post_commit = []
@@ -10,6 +12,7 @@ module EventStore::Hooks
     end
     
     def dispatch_deferred
+      Log.debug "Dispatching deferred commits..."
       @post_commit.each { |commit| @dispatcher_hook.post_commit(commit)  }
       @post_commit.clear
     end

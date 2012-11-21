4ff61f6e69c942c475e9d54099d1a73671f77d05
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-11-21 10:12:03 +0200

    Implementing deferred dispatcher hook.

--- lib/event-store/hooks/deferred-dispatcher-hook.rb
+++ lib/event-store/hooks/deferred-dispatcher-hook.rb
@@ -2,14 +2,16 @@ module EventStore::Hooks
   class DeferredDispatcherHook < PipelineHook
     def initialize(dispatcher_hook)
       @dispatcher_hook = dispatcher_hook
+      @post_commit = []
     end
     
     def post_commit(commit)
-      raise "Not implemented"
+      @post_commit << commit
     end
     
     def dispatch_deferred
-      raise "Not implemented"
+      @post_commit.each { |commit| @dispatcher_hook.post_commit(commit)  }
+      @post_commit.clear
     end
   end
 end
---
+++ spec/event-store/deferred-dispatcher-hook-spec.rb
@@ -0,0 +1,18 @@
+require 'spec-helper'
+
+describe EventStore::Hooks::DeferredDispatcherHook do
+  let(:original_hook) { mock(:original_hook) }
+  let(:commit_1) { mock(:commit_1) }
+  let(:commit_2) { mock(:commit_2) }
+  subject { described_class.new(original_hook) }
+  
+  it "should remember each commit on post_commit stage and dispatch it to original hook on dispatch_deferred" do
+    subject.post_commit(commit_1)
+    subject.post_commit(commit_2)
+    
+    original_hook.should_receive(:post_commit).with(commit_1)
+    original_hook.should_receive(:post_commit).with(commit_2)
+    
+    subject.dispatch_deferred
+  end
+end

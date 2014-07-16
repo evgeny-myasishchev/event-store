d01995da73a36bba4049cdded97a314ec4dbe4d3
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2014-07-16 18:01:28 +0300

    Assigning revision number and commit sequence to prevent test failures.

--- spec/support/commits-helper.rb
+++ spec/support/commits-helper.rb
@@ -6,16 +6,26 @@ module Support::CommitsHelper
   
   #*args are possible events
   def build_commit(stream_id, commit_id, *args, &block)
+    @stream_revision ||= {}
+    @stream_commit_sequence ||= {}
+
+    next_revision = (@stream_revision[stream_id] || 0) + (args.length == 0 ? 1 : args.length)
+    @stream_revision[stream_id] = next_revision
+    next_commit_sequence = (@stream_commit_sequence[stream_id] || 0) + 1
+    @stream_commit_sequence[stream_id] = next_commit_sequence
+
     commit_args = {
       :stream_id => stream_id,
       :commit_id => commit_id,
-      :events => args
+      :events => args,
+      :commit_sequence => next_commit_sequence,
+      :stream_revision => next_revision
     }
     yield commit_args if block_given?
     EventStore::Commit.new commit_args
   end
   
-  def commit_all(stream, *args)
-    args.each { |commit| stream.commit(commit) }
+  def commit_all(persistence, *args)
+    args.each { |commit| persistence.commit(commit) }
   end
 end

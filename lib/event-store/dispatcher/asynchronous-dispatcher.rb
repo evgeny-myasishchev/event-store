4b2c12540aa2bdb37a84e560de8f272d2886a448
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-09-10 00:15:09 +0300

    Implementing general asynchornous dispatcher logic.

--- lib/event-store/dispatcher/asynchronous-dispatcher.rb
+++ lib/event-store/dispatcher/asynchronous-dispatcher.rb
@@ -1,14 +1,33 @@
 module EventStore::Dispatcher
-  class AsynchronousDispatcher < Base
-    attr_reader :receiver
-    def initialize(&receiver)
-      @receiver = receiver
+  class AsynchronousDispatcher < SynchronousDispatcher
+    Log = EventStore::Logging::Logger.get 'event-store::dispatcher::asynchronous-dispatcher'
+    
+    def initialize(*args)
+      super
+      @queue = Queue.new
+      @worker = start_worker @queue
     end
     
-    #dispatch the specified commit to some kind of communications infrastructure.
-    def dispatch(commit)
-      @receiver.call(commit)
+    alias_method :super_schedule_dispatch, :schedule_dispatch
+    def schedule_dispatch(commit)
+      @queue.push(commit)
+    end
+    
+    # Sends a stop command and waits for all pending commits to be dispatched.
+    def stop
+      Log.info 'Stopping the asynchronous dispatcher...'
+      @queue.push(:stop)
+      @worker.join
+      Log.info 'Stopped.'
+    end
+    
+    private def start_worker queue
+      Log.info 'Starting asynchronous dispatcher worker thread...'
+      Thread.new do
+        until (commit = queue.pop) == :stop
+          super_schedule_dispatch commit
+        end
+      end
     end
   end
 end
-    
--- spec/event-store/dispatcher/asynchronous-dispatcher-spec.rb
+++ spec/event-store/dispatcher/asynchronous-dispatcher-spec.rb
@@ -1,5 +1,40 @@
 require 'spec-helper'
 
 describe EventStore::Dispatcher::AsynchronousDispatcher do
-  it 'is pending'
+  describe 'schedule_dispatch' do
+    it 'should dispatch the commit in an non-blocking way' do
+      commit = double(:commit)
+      is_dispatched = false
+      mutex = Mutex.new
+      resource = ConditionVariable.new
+      receiver = ->(c) { 
+        expect(c).to be commit
+        mutex.synchronize { 
+          is_dispatched = true 
+          resource.signal
+        }
+      }
+      subject = described_class.new &receiver
+      subject.schedule_dispatch commit
+      mutex.synchronize {
+        resource.wait(mutex, 2)
+        expect(is_dispatched).to be_truthy
+      }
+    end
+  end
+  
+  describe 'stop' do
+    it 'should stop the worker queue dispatching all pending commits blocking current thread' do
+      c1, c2 = double(:c1), double(:c2)
+      is_dispatched = false
+      receiver = ->(c) { 
+        is_dispatched = true if c == c2
+      }
+      subject = described_class.new &receiver
+      subject.schedule_dispatch c1
+      subject.schedule_dispatch c2
+      subject.stop
+      expect(is_dispatched).to be_truthy
+    end
+  end
 end

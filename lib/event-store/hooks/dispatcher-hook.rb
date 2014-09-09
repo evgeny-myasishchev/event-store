ccd57746e968d3c17f50f065d97ab4d395b313d9
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-09-09 10:47:14 +0300

    Hooking into the dispatcher pipeline to mark commit as dispatched on after_dispatch stage.

--- lib/event-store/hooks/dispatcher-hook.rb
+++ lib/event-store/hooks/dispatcher-hook.rb
@@ -6,14 +6,16 @@ module EventStore::Hooks
     
     def initialize(dispatcher, persistence_engine)
       @dispatcher, @persistence_engine = dispatcher, persistence_engine
+      @dispatcher.hook_into_pipeline after_dispatch: ->(commit) {
+        Log.debug "Marking commit '#{commit.commit_id}' as dispatched..."
+        persistence_engine.mark_commit_as_dispatched(commit)
+      }
     end
     
     #Dispatch the commit and mark it as dispatched on success
     def post_commit(commit)
       Log.debug "Dispatching commit '#{commit.commit_id}'..."
-      dispatcher.dispatch(commit)
-      Log.debug "Marking commit '#{commit.commit_id}' as dispatched..."
-      persistence_engine.mark_commit_as_dispatched(commit)
+      dispatcher.schedule_dispatch(commit)
     end
   end
 end
--- spec/event-store/dispatcher-hook-spec.rb
+++ spec/event-store/dispatcher-hook-spec.rb
@@ -1,23 +1,21 @@
 require 'spec-helper'
 
 describe EventStore::Hooks::DispatcherHook do
-  let(:commit) { double(:commit, :commit_id => "commit-1") }
-  let(:dispatcher) { double(:dispatcher) }
+  let(:commit) { double(:commit, :commit_id => 'commit-1') }
+  let(:dispatcher) { EventStore::Dispatcher::Base.new }
   let(:persistence_engine) { double(:persistence_engine) }
   let(:hook) { described_class.new dispatcher, persistence_engine }
   
-  describe "post_commit" do
-    it "should dispatch commit and mark it as dispatched on success" do
-      expect(dispatcher).to receive(:dispatch).with(commit)
-      expect(persistence_engine).to receive(:mark_commit_as_dispatched).with(commit)
+  it 'should hook into the dispatcher pipeline and mark commit as dispatched on after_dispatch' do
+    allow(dispatcher).to receive(:dispatch_immediately)
+    expect(persistence_engine).to receive(:mark_commit_as_dispatched).with(commit)
+    hook.post_commit(commit)
+  end
+  
+  describe 'post_commit' do
+    it 'should schedule dispatch' do
+      expect(dispatcher).to receive(:schedule_dispatch).with(commit)
       hook.post_commit(commit)
-    end
-    
-    it "should dispatch commit but don't mark it as dispached if failed" do
-      expect(dispatcher).to receive(:dispatch).with(commit).and_raise("some-error")
-      expect(persistence_engine).not_to receive(:mark_commit_as_dispatched).with(commit)
-      
-      expect(lambda { hook.post_commit(commit) }).to raise_error("some-error")
     end
   end
 end

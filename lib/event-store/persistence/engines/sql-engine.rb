a46f8db9a2e1316484966c8cb41e6edd02794a48
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-10-04 16:28:05 +0300

    Blob is not supported in postgres. Using File instead. In this case it's automatically mapped.

--- lib/event-store/persistence/engines/sql-engine.rb
+++ lib/event-store/persistence/engines/sql-engine.rb
@@ -105,8 +105,8 @@ module EventStore::Persistence::Engines
           Integer :stream_revision, :null=>false
           DateTime :commit_timestamp, :null=>false
           Boolean :has_been_dispatched, :null=>false, :default => false
-          Blob :events, :null=>false
-          Blob :headers, :null=>false
+          File :events, :null=>false
+          File :headers, :null=>false
           index [:stream_id, :commit_sequence], unique: true
           index [:stream_id, :stream_revision], unique: true
         end

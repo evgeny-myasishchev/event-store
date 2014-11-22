56d0117e4c05c8e263347214cf5403846dc51c46
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-11-23 01:33:21 +0200

    Adding large data failing specs

--- spec/integration/event-store-compression-spec.rb
+++ spec/integration/event-store-compression-spec.rb
@@ -2,10 +2,12 @@ require 'spec-helper'
 
 describe 'EventStore::Base integration' do
   subject {
-    EventStore::Bootstrap.bootstrap do |with|
-      with.sql_persistence(RSpec.configuration.database_config, {:orm_log_level => :debug}).compress.engine.purge
+    store = EventStore::Bootstrap.bootstrap do |with|
+      with.sql_persistence(RSpec.configuration.database_config, {:orm_log_level => :debug}).compress
       with.synchronous_dispatcher { |commit|  }
     end
+    store.purge
+    store
   }
   
   describe 'compression' do
@@ -21,6 +23,29 @@ describe 'EventStore::Base integration' do
       expect(opened_stream.committed_events.length).to eql 2
       expect(opened_stream.committed_events[0]).to eql evt1
       expect(opened_stream.committed_events[1]).to eql evt2
+    end
+    
+    it 'should persist correctly large events' do
+      evt = EventStore::EventMessage.new({
+        attrib1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        attrib2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      })
+      stream = subject.open_stream 'stream-222'
+      stream.add evt
+      stream.commit_changes
+      expect(subject.open_stream('stream-222').committed_events).to include evt
+    end
+    
+    it 'should persist correctly large headers' do
+      evt = EventStore::EventMessage.new({}, {
+        header1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        header1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      })
+      stream = subject.open_stream 'stream-222'
+      stream.add evt
+      stream.commit_changes
+      stream = subject.open_stream 'stream-222'
+      expect(subject.open_stream('stream-222').committed_events).to include evt
     end
   end
 end

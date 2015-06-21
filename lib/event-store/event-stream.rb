a81a0423d4a58feec72327db9b85c1ef7f3f0605
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-06-21 10:30:48 +0300

    Fixing indents.

--- lib/event-store/event-stream.rb
+++ lib/event-store/event-stream.rb
@@ -10,39 +10,39 @@ class EventStore::EventStream
   end
   
   # Gets the value which uniquely identifies the stream to which the stream belongs.
-	attr_reader :stream_id
+  attr_reader :stream_id
 	
   #Gets the value which indiciates the most recent committed revision of event stream.
   #Basically this value is the same as number of commited events in the stream. 
   #Value zero means there are no events (and commits) in the stream.
-	attr_reader :stream_revision
+  attr_reader :stream_revision
 
   #Gets the value which indicates the most recent committed sequence identifier of the event stream.
   #This value is incremented each time new commit is persisted.
   #Value zero means there are commits in the stream.
-	attr_reader :commit_sequence
+  attr_reader :commit_sequence
 
   #Gets the collection of events which have been successfully persisted to durable storage.
-	def committed_events
-	  @committed_events_ro ||= EventStore::Infrastructure::ReadOnlyArray.new(@committed_events) 
-	end
+  def committed_events
+    @committed_events_ro ||= EventStore::Infrastructure::ReadOnlyArray.new(@committed_events) 
+  end
 
-	#Gets the collection of yet-to-be-committed events that have not yet been persisted to durable storage.
-	def uncommitted_events
-	  @uncommitted_events_ro ||= EventStore::Infrastructure::ReadOnlyArray.new(@uncommitted_events) 
-	end
+  #Gets the collection of yet-to-be-committed events that have not yet been persisted to durable storage.
+  def uncommitted_events
+    @uncommitted_events_ro ||= EventStore::Infrastructure::ReadOnlyArray.new(@uncommitted_events) 
+  end
 	
-	#
-	# Args:
-	# * stream_id - stream identifier. Can be generated with Identity::generate
-	# * persistence_engine - the engine to access commits	
-	#
-	# options:
-	# * hooks - pipeline hooks that are invoked at different stages. See PipelineHook.
-	# * 
-	# 
-	def initialize(stream_id, persistence_engine, min_revision: nil, hooks: [])
-	  raise InvalidStreamIdError.new "stream_id can not be null or empty" if stream_id == nil || stream_id == ""
+  #
+  # Args:
+  # * stream_id - stream identifier. Can be generated with Identity::generate
+  # * persistence_engine - the engine to access commits	
+  #
+  # options:
+  # * hooks - pipeline hooks that are invoked at different stages. See PipelineHook.
+  # * 
+  # 
+  def initialize(stream_id, persistence_engine, min_revision: nil, hooks: [])
+    raise InvalidStreamIdError.new "stream_id can not be null or empty" if stream_id == nil || stream_id == ""
     @stream_id          = stream_id
     @persistence_engine = persistence_engine
     @uncommitted_events = []
@@ -54,8 +54,8 @@ class EventStore::EventStream
     @uncommitted_events = []
     
     commits = min_revision.nil? ? 
-      persistence_engine.get_from(stream_id) :
-      persistence_engine.get_from(stream_id, min_revision: min_revision)
+    persistence_engine.get_from(stream_id) :
+    persistence_engine.get_from(stream_id, min_revision: min_revision)
     if commits.empty?
       head = persistence_engine.get_head(stream_id) if min_revision
       if head && head[:commit_sequence] && head[:stream_revision]
@@ -74,42 +74,42 @@ class EventStore::EventStream
     end
   end
 
-	# Adds the event messages provided to the session to be tracked.
-	#  event - The event-message to be tracked.
-	def add(uncommitted_event) 
-	  @uncommitted_events.push(uncommitted_event)
-	  self
-	end
+  # Adds the event messages provided to the session to be tracked.
+  #  event - The event-message to be tracked.
+  def add(uncommitted_event) 
+    @uncommitted_events.push(uncommitted_event)
+    self
+  end
 
-	# Commits the changes to durable storage.
-	#  => returns commit
-	def commit_changes(transaction_context, headers = {})
-	  unless uncommitted_events.length > 0
-	    Log.warn "No uncommitted events found for stream '#{stream_id}'. Commit skipped."
-	    return
-	  end
-	  Log.debug "Committing '#{stream_id}'. #{@uncommitted_events.length} uncommitted events to commit."
-	  attempt = EventStore::Commit.build(self, @uncommitted_events.dup, headers)
-	  @persistence_engine.commit(transaction_context, attempt)
+  # Commits the changes to durable storage.
+  #  => returns commit
+  def commit_changes(transaction_context, headers = {})
+    unless uncommitted_events.length > 0
+      Log.warn "No uncommitted events found for stream '#{stream_id}'. Commit skipped."
+      return
+    end
+    Log.debug "Committing '#{stream_id}'. #{@uncommitted_events.length} uncommitted events to commit."
+    attempt = EventStore::Commit.build(self, @uncommitted_events.dup, headers)
+    @persistence_engine.commit(transaction_context, attempt)
     @new_stream = false #After commits are committed the stream is not new anymore
-	  populate_stream_with([attempt])
-	  attempt.events.each { |evt| @uncommitted_events.delete(evt) }
+    populate_stream_with([attempt])
+    attempt.events.each { |evt| @uncommitted_events.delete(evt) }
     transaction_context.after_commit {
       Log.debug "Processing pipeline hooks..."
       @pipeline_hooks.each { |hook| hook.post_commit(attempt) }
     }
-	  Log.debug "Done."
-	  attempt
-	end
-	
-	private
-	  def populate_stream_with(commits, min_revision: nil)
+    Log.debug "Done."
+    attempt
+  end
+  
+  private
+    def populate_stream_with(commits, min_revision: nil)
       Log.debug "Populating stream '#{stream_id}' with #{commits.length} commits..."
       Log.debug "Min revision limit specified: #{min_revision}" unless min_revision.nil?
       @new_stream = false
       last_commit = nil
       first_commit = commits.first
-	    commits.each do |commit|
+      commits.each do |commit|
         last_commit = commit
         events = commit.events
         # Only some slice of the events from the first commit may have to be retrieved
@@ -123,9 +123,9 @@ class EventStore::EventStream
             events = events[slice_start..slice_end]
           end
         end
-	      @committed_events.concat(events)
-	    end
+        @committed_events.concat(events)
+      end
       @stream_revision = last_commit.stream_revision
       @commit_sequence = last_commit.commit_sequence
-	  end
+    end
 end

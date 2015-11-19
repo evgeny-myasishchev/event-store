dcd676372387d1c26171b331ce10d3141a74f3ea
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-11-19 15:01:22 +0200

    Moving serialization logic to a base class since it's quite the same.

--- lib/event-store/persistence/serializers.rb
+++ lib/event-store/persistence/serializers.rb
@@ -1,33 +1,25 @@
 module EventStore::Persistence::Serializers
   class AbstractSerializer
     def serialize(object)
-      raise 'Not implemented'
-    end
-    
-    def deserialize(data)
-      raise 'Not implemented'
-    end
-  end
-  
-  class MarshalSerializer < AbstractSerializer
-    def serialize(object)
-      Marshal.dump(object)
-    end
-    
-    def deserialize(data)
-      Marshal.load(data)
-    end
-  end
-  
-  class JsonSerializer < AbstractSerializer
-    def serialize(object)
       self.class.serializer.dump(object)
     end
     
     def deserialize(data)
       self.class.serializer.load(data)
     end
-    
+
+    def self.serializer
+      raise 'Not implemented'
+    end
+  end
+  
+  class MarshalSerializer < AbstractSerializer
+    def self.serializer
+      Marshal
+    end
+  end
+  
+  class JsonSerializer < AbstractSerializer
     def self.serializer
       require 'json' unless defined?(JSON)
       return JSON
@@ -35,14 +27,6 @@ module EventStore::Persistence::Serializers
   end
   
   class YamlSerializer < AbstractSerializer
-    def serialize(object)
-      self.class.serializer.dump(object)
-    end
-    
-    def deserialize(data)
-      self.class.serializer.load(data)
-    end
-    
     def self.serializer
       require 'yaml' unless defined?(YAML)
       return YAML
